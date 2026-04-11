3c3cadea5aafa65c257e73fe8d40eccf33b84d92
FIRST_NAME LAST_NAME <LessandraTerapias@lessaterapias12@gmail.com>
2026-04-11 12:27:10 +0200

    add menu button

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
        document.body.style.opacity = 1;
    }, 100);
 });
+
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("show");
+}
+
+// Cerrar si haces click fuera
+window.onclick = function(e) {
+  if (!e.target.matches('.dropdown-btn')) {
+    const menu = document.getElementById("menu");
+    if (menu.classList.contains('show')) {
+      menu.classList.remove('show');
+    }
+  }
+}
